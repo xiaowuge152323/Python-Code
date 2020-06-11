931d1539cf07c666354720c2268ebb0197ead416
xiaowuge152323 <1031395504@qq.com>
2020-06-11 11:31:58 +0800

    更新了文件

---
+++ 第一章/代码清单1-1.py
@@ -0,0 +1,4 @@
+print "I love pizza!"
+print "pizza " * 20
+print "yun " * 40
+print "I'm full."
---
+++ 第一章/你的第二个程序.py
@@ -0,0 +1,12 @@
+AHOY! I'm the Dread Pirate Roberts, and I have a secret!
+It is a number from l to 99. I'll give you 6 tries.
+Whai's yer guess? 40
+Too high, landlubber!
+What's yer guess? 20
+Too high, landlubber!
+What's ter guess? 10
+Too low, ye scurvy dog !
+What's ter guess? 11
+Too low, ye scurvy dog !
+What's yer guess? 12
+Avast! ye got it! Found mysecret, ye did!
