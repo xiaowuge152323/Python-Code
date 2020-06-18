1c6ad2a05cd6d06e87db8a2b40388035e65b1be9
xiaowuge152323 <1031395504@qq.com>
2020-06-18 10:48:31 +0800

    12

---
+++ 第1章/代码清单1-2.py
@@ -0,0 +1,20 @@
+import random
+secret = random.randint(1,9)
+guess = 0
+tries = 0
+print "AHOY! I'm the Dread Pirate Roberts, and I have a secret!"
+print "It is a number from l to 99. I'll give you 6 tries. "
+while guess != secret and tries < 6:
+      guess = input("What's yer guess? ")
+      if guess < secret:
+          print "Too low, ye scurvy dog!"
+      elif guess > secret:
+          print "Too high, landlubber!"
+
+      tries = tries + 1
+
+if guess == secret:
+    print "Avast! ye got it! Found my secret, ye did!"
+else:
+    print "No more guesses! Better luck next time, matey!"
+    print "The secret number was", secret
